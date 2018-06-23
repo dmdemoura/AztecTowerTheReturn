d316c83e5bcf0afb952a0576e2d1a5fa6b7181e1
Gabriel Moura <gdeperes@gmail.com>
2018-06-23 02:28:05 -0300

    Added Fire Tower and Prefab folder

---
+++ Assets/Scripts/FireTower.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireTower : MonoBehaviour {
+    [SerializeField] private GameObject flame;
+    [SerializeField] private float flameSpeed;
+
+	private void FixedUpdate () {
+        flame.transform.RotateAround(transform.position, new Vector3(0,0, 1), flameSpeed);
+	}
+}
