5c7c9c57311bbd40172c5de8db04a5b92ff84e5e
Abner Eduardo Silveira Santos <abner.santos@usp.br>
2018-06-23 04:07:47 -0300

    Enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,16 +1,94 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
+	List<GameObject> targets = new List<GameObject>();
+	GameObject currentTarget;
+	Vector2 targetPos;
+	bool isAttacking = false;
+	enum FacingSide {Left, Right};
+	FacingSide facingDirection;
+
+	[SerializeField]
+	float speed, xMoveOffset, yMoveOffset, hitRange, attackDelay, enemyDamage;
+
 	// Use this for initialization
-	void Start () {
+	void Awake()
+	 {
+		targets.AddRange(GameObject.FindGameObjectsWithTag("Tower"));
+		targets.Add(GameObject.FindGameObjectWithTag("Player"));
+
+		List<float> distances = new List<float>();
+		foreach(var target in targets)
+			distances.Add(Vector2.Distance(target.transform.position, this.transform.position));
 		
-	}
+		currentTarget = targets[distances.IndexOf(distances.Min())];
+		if(currentTarget.tag == "Player")
+			InvokeRepeating("TrackMovingTarget", 0f, Time.deltaTime);
+		else
+		{
+			targetPos = currentTarget.transform.position;
+			InvokeRepeating("TrackStillTarget", 0f, Time.deltaTime);
+		}
+	}		
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void TrackStillTarget()
+	{
+		Debug.Log("Yo1");
+		if(!isAttacking)
+		{
+			if(Mathf.Abs(((Vector2)this.transform.position - targetPos).x) > xMoveOffset
+			|| Mathf.Abs(((Vector2)this.transform.position - targetPos).y) > yMoveOffset)
+				this.transform.position = Vector2.MoveTowards(this.transform.position, targetPos, speed);
+			else
+				AttemptAttack();
+		}
+	}
+
+	void TrackMovingTarget()
+	{
+		Debug.Log("Yo2");
+		targetPos = currentTarget.transform.position;
+		Debug.Log(Mathf.Abs(((Vector2)this.transform.position - targetPos).x));
+
+		if(!isAttacking)
+		{
+			if(Mathf.Abs(((Vector2)this.transform.position - targetPos).x) > xMoveOffset
+			|| Mathf.Abs(((Vector2)this.transform.position - targetPos).y) > yMoveOffset)
+				this.transform.position = Vector2.MoveTowards(this.transform.position, targetPos, speed);
+			else
+				AttemptAttack();
+		}
+	}
+
+	IEnumerator AttemptAttack()
+	{
+		isAttacking = true;
+
+		Vector2 hitDirection;
+
+		if(facingDirection == FacingSide.Left)
+			hitDirection = Vector2.left;
+		else
+			hitDirection = Vector2.right;
+
+		RaycastHit2D hit = Physics2D.Raycast(this.transform.position, hitDirection, hitRange);
+		if(hit)
+		{
+			yield return new WaitForSeconds(attackDelay);
+			if(hit.transform.gameObject.tag != "Enemy")
+				hit.transform.gameObject.SendMessage("GetHit", enemyDamage);
+		}
+		isAttacking = false;
+	}
+
+	void DrawAggro()
+	{
+		CancelInvoke("TrackStillTarget");
+		currentTarget = GameObject.FindGameObjectWithTag("Player");
+		InvokeRepeating("TrackMovingTarget", 0f, Time.deltaTime);
 	}
 }
